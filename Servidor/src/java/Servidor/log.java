2618660ed521588087dc88aefc791ff748611b07
Emanuel Rodrigues <emanuelarroja1998@gmail.com>
2019-07-06 23:42:50 +0100

    Create log.java

---
+++ Servidor/src/java/Servidor/log.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servidor;
+
+/**
+ *
+ * @author Emanuel Rodrigues
+ */
+public class log {
+    
+    
+    private String msg;
+    private int id;
+
+    public log(String msg, int id) {
+        this.msg = msg;
+        this.id = id;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
+}
