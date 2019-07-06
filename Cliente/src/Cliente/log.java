07a38a32207d3f126935ede773ab814b9f7a9e75
Emanuel Rodrigues <emanuelarroja1998@gmail.com>
2019-07-07 00:17:02 +0100

    Create log.java

---
+++ Cliente/src/Cliente/log.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cliente;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Emanuel Rodrigues
+ */
+@XmlRootElement(name = "log")
+public class log {
+    
+    
+    private String msg;
+    private int id;
+    //construtor
+    public log() {
+    }
+
+
+    public log(String msg, int id) {
+        this.msg = msg;
+        this.id = id;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+    
+    @XmlAttribute
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
+}
